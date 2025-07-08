6f9ff3fd64400b354b8c8a7a56c337996145ccdc
Jk Dev <jktripuri@gmail.com>
2025-07-08 15:48:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,15 +3,14 @@ import os
 from dotenv import load_dotenv
 from commands import handle_command, setup_help_command
 from keep_alive import keep_alive
-from automod import handle_automod_message # Import the automod function
+# from automod import handle_automod_message # COMMENT THIS OUT
+# MOD_CHANNEL_ID = int(os.getenv("MOD_CHANNEL_ID")) # COMMENT THIS OUT
 
 keep_alive()
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
-# Define your moderation channel ID. You might want to add this to your .env file too.
-MOD_CHANNEL_ID = int(os.getenv("MOD_CHANNEL_ID")) # Make sure to add MOD_CHANNEL_ID to your .env file!
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -35,14 +34,10 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    # First, let automod check the message
-    # If automod handles the message (e.g., deletes it), it will return True.
-    # In that case, we don't want to process it further for commands.
-    automod_handled = await handle_automod_message(client, message, MOD_CHANNEL_ID)
-    if automod_handled:
-        return # Stop processing if automod took action
+    # automod_handled = await handle_automod_message(client, message, MOD_CHANNEL_ID) # COMMENT THIS OUT
+    # if automod_handled: # COMMENT THIS OUT
+    #     return # COMMENT THIS OUT
 
-    # Continue with your existing command handling only if automod didn't act
     if message.author.id != OWNER_ID:
         return
     await handle_command(client, message, send_status)
